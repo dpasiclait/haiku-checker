cad8f4188754f5447ce874021c56ae381bac69f2
Djidjelly Philippe Arnault Siclait <dpasiclait@gmail.com>
2021-02-23 19:42:56 -0400

    create app routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HaikuCheckerComponent } from "./haiku/haiku-checker/haiku-checker.component";
+import { HaikuFactsComponent } from "./haiku/haiku-facts/haiku-facts.component";
+
+const routes: Routes = [
+  { path: '', component: HaikuCheckerComponent },
+  { path: 'facts', component: HaikuFactsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
