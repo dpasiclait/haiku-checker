916239c3dff1d44e946ef89c4001207c4e7eb738
Djidjelly Philippe Arnault Siclait <dpasiclait@gmail.com>
2021-02-23 19:44:03 -0400

    create basic html outline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HaikuModule } from './haiku/haiku.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    HaikuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
