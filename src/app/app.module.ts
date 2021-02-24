ae9c441b8365f95183a3278331e10ffb5d400daa
Djidjelly Philippe Arnault Siclait <dpasiclait@gmail.com>
2021-02-23 20:18:01 -0400

    add angular material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HaikuModule } from './haiku/haiku.module';
 import { HeaderComponent } from './header/header.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,11 @@ import { HeaderComponent } from './header/header.component';
     FooterComponent,
   ],
   imports: [
+    AngularMaterialModule,
     AppRoutingModule,
     BrowserModule,
-    HaikuModule
+    HaikuModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
