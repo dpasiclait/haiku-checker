d349139fac57a80e390b648890e12a2c6ee11122
Djidjelly Philippe Arnault Siclait <dpasiclait@gmail.com>
2021-04-03 11:26:36 -0400

    replace fontawesome url link to static integrated css files in assets

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,7 +1,6 @@
 import {
   Component
 } from "@angular/core";
-import { environment } from "src/environments/environment.prod";
 
 @Component({
   selector   : 'app-footer',
@@ -9,8 +8,5 @@ import { environment } from "src/environments/environment.prod";
   styleUrls  : ['./footer.component.css']
 })
 export class FooterComponent {
-  github = `${environment.AWS3Name}/github`;
-  linkedIn = `${environment.AWS3Name}/linkedIn`;
-  // github = `http://localhost:4200/github`;
-  // linkedIn = `http://localhost:4200/linkedIn`;
+
 }
