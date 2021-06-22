91eeae7b0b64f85461e148d545d9bf099033b4be
Djidjelly Philippe Arnault Siclait <dpasiclait@gmail.com>
2021-06-21 20:24:44 -0400

    refactor header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { environment } from "src/environments/environment.prod";
 @Component({
   selector   : 'app-header',
   templateUrl: './header.component.html',
-  styleUrls  : ['./header.component.css']
+  styleUrls  : ['./header.component.css', '../../assets/css/color-scheme.css']
 })
 export class HeaderComponent {
   version: string = environment.Version;
