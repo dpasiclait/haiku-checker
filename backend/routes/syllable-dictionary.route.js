bd14068e734c8f1a9a0f5460fefe65bd1f7c5fc7
Djidjelly Philippe Arnault Siclait <dpasiclait@gmail.com>
2021-07-11 16:06:18 -0400

    testing swagger ui

--- backend/routes/syllable-dictionary.route.js
+++ backend/routes/syllable-dictionary.route.js
@@ -4,27 +4,71 @@ const SyllableDictionaryController = require('../controllers/syllable-dictionary
 const router                       = express.Router();
 
 // TODO: ADD SWAGGER UI
-/**
- * @swagger
- * /api/syllable_dictionary/upload_data:
- *   get:
- *     summary: Retrieve a list of JSONPlaceholder users
- *     description: Yolo
- *     responses:
- *       '200':
- *          description: Yolo
-*/
 router.get('/upload_data', SyllableDictionaryController.uploadSyllablesToDictionary);
 
 /**
  * @swagger
  * /api/syllable_dictionary/:
  *   post:
- *     summary: Retrieve a list of JSONPlaceholder users
- *     description: Yolo
+ *     summary: Verifies a the syllable count of a haiku
+ *     description: This service receives an input object with three verses to count the syllables of each line
+ *     requestBody:
+ *       required: true
+ *       content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              firstVerse:
+ *                type: string
+ *                example: "This is the first line"
+ *              secondVerse:
+ *                type: string
+ *                example: "Followed by the second line"
+ *              thirdVerse:
+ *                type: string
+ *                example: "With the third to end"
  *     responses:
  *       '200':
- *         description: Yolo
+ *         description: Successfully returns the three verses' syllable count and a list of all unknown words
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 payload:
+ *                   type: object
+ *                   properties:
+ *                     firstVerseCount:
+ *                       type: number
+ *                       example: 5
+ *                     secondVerseCount:
+ *                       type: number
+ *                       example: 7
+ *                     thirdVerseCount:
+ *                       type: number
+ *                       example: 5
+ *                     unknownWords:
+ *                       type: array
+ *                       example: ['unknown_word1', 'unknown_word2']
+ *                 timestamp:
+ *                  type: number
+ *                  example: 1626026131764
+ *       '400':
+ *         description: One verse was empty upon reception
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 payload:
+ *                   type: string
+ *                   example: "At least one verse is missing"
+ *                 timestamp:
+ *                   type: number
+ *                   example: 1626026131764
+ *       '500':
+ *         description: Internal Server Error
 */
 router.post('/', SyllableDictionaryController.verifyHaiku);
 
